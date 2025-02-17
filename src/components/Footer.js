2456ac0dfe65e0956e1de7e3c9016b2609434c6c
Jyoti Anand <jyotianand2112@gmail.com>
2025-02-17 13:54:32 +0530

    t push origin mainSecond commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,11 @@ function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4 mt-8">
       <div className="container mx-auto px-4 text-center">
+<<<<<<< HEAD
         <p>&copy; 2025 iScrape. Made with ❤️ from Jyoti  .</p>
+=======
+        <p>&copy; 2024 iScrape. Made with ❤️ from Jyoti.</p>
+>>>>>>> cac11b7 (Second commit)
       </div>
     </footer>
   );
